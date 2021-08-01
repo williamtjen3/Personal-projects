3f3cda8e83e33f5b803ecafb51a5aed29fc651cb
William Tjen <willy.tjen@gmail.com>
2021-08-01 16:39:18 -0400

    remove election project

--- election dataset/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-
-primary_df = pd.read_csv("us-2016-primary-results.csv", delimiter=";")
-election_df = pd.read_csv("usa-2016-presidential-election-by-county.csv", delimiter=";")
-
-primary_df.info()
-election_df.info()
