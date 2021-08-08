c3812f87c12fc776b54b0bb8328f1c053a7026ee
William Tjen <willy.tjen@gmail.com>
2021-08-08 18:43:05 -0400

    web dev cont

--- Web development/django/django_project/blog/views.py
+++ Web development/django/django_project/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'William Tjen',
+        'title' : 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 08, 2021'
+    },
+    {
+        'author': 'Evelyn Leonard',
+        'title' : 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 07, 2021'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html')
