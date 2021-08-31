cf51b3107f2b00b760adc4b626e867884099eb98
William Tjen <willy.tjen@gmail.com>
2021-08-30 23:07:27 -0400

    data viz project

--- Web development/django/data viz/data_viz/data_viz/wsgi.py
+++ Web development/django/data viz/data_viz/data_viz/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for evelynchatapp project.
+WSGI config for data_viz project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'evelynchatapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'data_viz.settings')
 
 application = get_wsgi_application()
